28d0202d0b86b69624324989f09c0ac002978605
Talisson-matos <talissonmatos23@gmail.com>
2025-08-21 10:50:02 -0300

    agileTrucker2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -320,23 +320,23 @@ function App() {
                     value={creditoPresumido}
                     onChange={(e) => setCreditoPresumido(e.target.value)}
                   >
-                    <option value="">Selecione uma opção</option>
-                    <option value="CREDITO PRESUMIDO REF. EMBASAMENTO LEGAL ART. 107 INCISO 37 CONVÊNIO 106/96">ESTADO MG</option>
-                    <option value="CREDITO PRESUMIDO CONF. CONVÊNIO ICMS 106/96 COMBINADO AO § 3º PELO CONV. ICMS 85/03">ESTADO PA</option>
-                    <option value="ANEXO I AO DECRETO Nº 18.955, DE 22 DE DEZEMBRO DE 1997 - CADERNO III">ESTADO DF</option>
-                    <option value="BASE DE CALCULO DO ICMS REDUZIDA, CONFORME ART. 87, INCISO XX-B, DO RICMS.">ESTADO RN</option>
-                    <option value="CONVÊNIO ICMS 85/2003 ART. 3.2 DO REGULAMENTO DO PARANÁ">ESTADO PR</option>
-                    <option value="CREDITO PRESUMIDO CONF. CONVÊNIO ICMS 106/96 COMBINADO AO INCISO IV, DO ART. 57 DO RICMS-SE">ESTADO SE</option>
-                    <option value="CREDITO PRESUMIDO CONF. CONVÊNIO ICMS 106/96 COMBINADO AO INCISO IX, DO ART. 1 DO ANEXO 1.5 DO ANEXO 1.0 DO RICMS-MA">ESTADO MA</option>
-                    <option value="CREDITO PRESUMIDO CONF. CONVÊNIO ICMS 106/96 COMBINADO AO DECRETO Nº 24.333/97, E NO INCISO V DO ART. 64 DO DECRETO Nº 24.569, DE 31 DE JULHO DE 1997.">ESTADO CE</option>
-                    <option value="CREDITO PRESUMIDO CONF. CONVÊNIO ICMS 106/96 COMBINADO A ALINEA 'B' DO INCISO III DO ART. 270 DO RICMS/BA">ESTADO BA</option>
-                    <option value="CREDITO PRESUMIDO CONF. DECRETO Nº 27815 DE 24/01/2001 COMBINADO AO CONVÊNIO ICMS 106/96.">ESTADO RJ</option>
-                    <option value="CREDITO PRESUMIDO - TRANSPORTADORA NÃO OBRIGADO A INSCRIÇÃO DENTRO DO ESTADO DO PE CONF. ALÍNEA 'E' DO INC XI DO ART. 35 DO DECRETO 14876/91.">ESTADO PE</option>
-                    <option value="CREDITO PRESUMIDO - PRESTADOR DE SERVIÇO DE TRANSPORTE NÃO OBRIGADO À INSCRIÇÃO NO CCICMS - CONF. § 3º, ART. 25 DO ANEXO II DO RICMS/SC">ESTADO SC</option>
-                    <option value="CREDITO PRESUMIDO - PRESTADOR NÃO OBRIGADO À INSCRIÇÃO NESTA UF. ALINEA C, INC. III, ART. 56 DO RICMS/PI">ESTADO PI</option>
-                    <option value="CREDITO PRESUMIDO - INCISO II, § 1º, DO ARTIGO 64 DO RCTE-GO">ESTADO GO</option>
-                    <option value="CREDITO PRESUMIDO - INCISO 37 DO ARTIGO 107 DO RICMS/ES">ESTADO ES</option>
-                    <option value="CREDITO PRESUMIDO - INCISO § 3º DO ARTIGO 11 DO ANEXO III DO RICMS-SP">ESTADO SP</option>
+                    <option value="">SELECIONAR UF CRED. PRESUMIDO - (REMETENTE)</option>
+                    <option value="CREDITO PRESUMIDO REF. EMBASAMENTO LEGAL ART. 107 INCISO 37 CONVÊNIO 106/96">MG - MINAS GERAIS</option>
+                    <option value="CREDITO PRESUMIDO CONF. CONVÊNIO ICMS 106/96 COMBINADO AO § 3º PELO CONV. ICMS 85/03">PA - PARÁ</option>
+                    <option value="ANEXO I AO DECRETO Nº 18.955, DE 22 DE DEZEMBRO DE 1997 - CADERNO III">DF - DISTRITO FEDEDERAL</option>
+                    <option value="BASE DE CALCULO DO ICMS REDUZIDA, CONFORME ART. 87, INCISO XX-B, DO RICMS.">RN - RIO GRANDE DO NORTE</option>
+                    <option value="CONVÊNIO ICMS 85/2003 ART. 3.2 DO REGULAMENTO DO PARANÁ">PR - PARANÁ</option>
+                    <option value="CREDITO PRESUMIDO CONF. CONVÊNIO ICMS 106/96 COMBINADO AO INCISO IV, DO ART. 57 DO RICMS-SE">SE - SERGIPE</option>
+                    <option value="CREDITO PRESUMIDO CONF. CONVÊNIO ICMS 106/96 COMBINADO AO INCISO IX, DO ART. 1 DO ANEXO 1.5 DO ANEXO 1.0 DO RICMS-MA">MA - MARANHÃO</option>
+                    <option value="CREDITO PRESUMIDO CONF. CONVÊNIO ICMS 106/96 COMBINADO AO DECRETO Nº 24.333/97, E NO INCISO V DO ART. 64 DO DECRETO Nº 24.569, DE 31 DE JULHO DE 1997.">CE - CEARÁ</option>
+                    <option value="CREDITO PRESUMIDO CONF. CONVÊNIO ICMS 106/96 COMBINADO A ALINEA 'B' DO INCISO III DO ART. 270 DO RICMS/BA">BA - BAHIA</option>
+                    <option value="CREDITO PRESUMIDO CONF. DECRETO Nº 27815 DE 24/01/2001 COMBINADO AO CONVÊNIO ICMS 106/96.">RJ - RIO DE JANEIRO</option>
+                    <option value="CREDITO PRESUMIDO - TRANSPORTADORA NÃO OBRIGADO A INSCRIÇÃO DENTRO DO ESTADO DO PE CONF. ALÍNEA 'E' DO INC XI DO ART. 35 DO DECRETO 14876/91.">PE - PERNAMBUCO</option>
+                    <option value="CREDITO PRESUMIDO - PRESTADOR DE SERVIÇO DE TRANSPORTE NÃO OBRIGADO À INSCRIÇÃO NO CCICMS - CONF. § 3º, ART. 25 DO ANEXO II DO RICMS/SC">SC - SANTA CATARINA</option>
+                    <option value="CREDITO PRESUMIDO - PRESTADOR NÃO OBRIGADO À INSCRIÇÃO NESTA UF. ALINEA C, INC. III, ART. 56 DO RICMS/PI">PI - PIAUÍ</option>
+                    <option value="CREDITO PRESUMIDO - INCISO II, § 1º, DO ARTIGO 64 DO RCTE-GO">GO - GOIÁS</option>
+                    <option value="CREDITO PRESUMIDO - INCISO 37 DO ARTIGO 107 DO RICMS/ES">ES - ESPIRITO SANTO</option>
+                    <option value="CREDITO PRESUMIDO - INCISO § 3º DO ARTIGO 11 DO ANEXO III DO RICMS-SP">SP - SÃO PAULO</option>
                   </select>
 
                   
@@ -408,23 +408,23 @@ function App() {
                     value={creditoPresumido}
                     onChange={(e) => setCreditoPresumido(e.target.value)}
                   >
-                    <option value="">Selecione uma opção</option>
-                    <option value="CREDITO PRESUMIDO REF. EMBASAMENTO LEGAL ART. 107 INCISO 37 CONVÊNIO 106/96">ESTADO MG</option>
-                    <option value="CREDITO PRESUMIDO CONF. CONVÊNIO ICMS 106/96 COMBINADO AO § 3º PELO CONV. ICMS 85/03">ESTADO PA</option>
-                    <option value="ANEXO I AO DECRETO Nº 18.955, DE 22 DE DEZEMBRO DE 1997 - CADERNO III">ESTADO DF</option>
-                    <option value="BASE DE CALCULO DO ICMS REDUZIDA, CONFORME ART. 87, INCISO XX-B, DO RICMS.">ESTADO RN</option>
-                    <option value="CONVÊNIO ICMS 85/2003 ART. 3.2 DO REGULAMENTO DO PARANÁ">ESTADO PR</option>
-                    <option value="CREDITO PRESUMIDO CONF. CONVÊNIO ICMS 106/96 COMBINADO AO INCISO IV, DO ART. 57 DO RICMS-SE">ESTADO SE</option>
-                    <option value="CREDITO PRESUMIDO CONF. CONVÊNIO ICMS 106/96 COMBINADO AO INCISO IX, DO ART. 1 DO ANEXO 1.5 DO ANEXO 1.0 DO RICMS-MA">ESTADO MA</option>
-                    <option value="CREDITO PRESUMIDO CONF. CONVÊNIO ICMS 106/96 COMBINADO AO DECRETO Nº 24.333/97, E NO INCISO V DO ART. 64 DO DECRETO Nº 24.569, DE 31 DE JULHO DE 1997.">ESTADO CE</option>
-                    <option value="CREDITO PRESUMIDO CONF. CONVÊNIO ICMS 106/96 COMBINADO A ALINEA 'B' DO INCISO III DO ART. 270 DO RICMS/BA">ESTADO BA</option>
-                    <option value="CREDITO PRESUMIDO CONF. DECRETO Nº 27815 DE 24/01/2001 COMBINADO AO CONVÊNIO ICMS 106/96.">ESTADO RJ</option>
-                    <option value="CREDITO PRESUMIDO - TRANSPORTADORA NÃO OBRIGADO A INSCRIÇÃO DENTRO DO ESTADO DO PE CONF. ALÍNEA 'E' DO INC XI DO ART. 35 DO DECRETO 14876/91.">ESTADO PE</option>
-                    <option value="CREDITO PRESUMIDO - PRESTADOR DE SERVIÇO DE TRANSPORTE NÃO OBRIGADO À INSCRIÇÃO NO CCICMS - CONF. § 3º, ART. 25 DO ANEXO II DO RICMS/SC">ESTADO SC</option>
-                    <option value="CREDITO PRESUMIDO - PRESTADOR NÃO OBRIGADO À INSCRIÇÃO NESTA UF. ALINEA C, INC. III, ART. 56 DO RICMS/PI">ESTADO PI</option>
-                    <option value="CREDITO PRESUMIDO - INCISO II, § 1º, DO ARTIGO 64 DO RCTE-GO">ESTADO GO</option>
-                    <option value="CREDITO PRESUMIDO - INCISO 37 DO ARTIGO 107 DO RICMS/ES">ESTADO ES</option>
-                    <option value="CREDITO PRESUMIDO - INCISO § 3º DO ARTIGO 11 DO ANEXO III DO RICMS-SP">ESTADO SP</option>
+                    <option value="">SELECIONAR UF CRED. PRESUMIDO - (REMETENTE)</option>
+                    <option value="CREDITO PRESUMIDO REF. EMBASAMENTO LEGAL ART. 107 INCISO 37 CONVÊNIO 106/96">MG - MINAS GERAIS</option>
+                    <option value="CREDITO PRESUMIDO CONF. CONVÊNIO ICMS 106/96 COMBINADO AO § 3º PELO CONV. ICMS 85/03">PA - PARÁ</option>
+                    <option value="ANEXO I AO DECRETO Nº 18.955, DE 22 DE DEZEMBRO DE 1997 - CADERNO III">DF - DISTRITO FEDEDERAL</option>
+                    <option value="BASE DE CALCULO DO ICMS REDUZIDA, CONFORME ART. 87, INCISO XX-B, DO RICMS.">RN - RIO GRANDE DO NORTE</option>
+                    <option value="CONVÊNIO ICMS 85/2003 ART. 3.2 DO REGULAMENTO DO PARANÁ">PR - PARANÁ</option>
+                    <option value="CREDITO PRESUMIDO CONF. CONVÊNIO ICMS 106/96 COMBINADO AO INCISO IV, DO ART. 57 DO RICMS-SE">SE - SERGIPE</option>
+                    <option value="CREDITO PRESUMIDO CONF. CONVÊNIO ICMS 106/96 COMBINADO AO INCISO IX, DO ART. 1 DO ANEXO 1.5 DO ANEXO 1.0 DO RICMS-MA">MA - MARANHÃO</option>
+                    <option value="CREDITO PRESUMIDO CONF. CONVÊNIO ICMS 106/96 COMBINADO AO DECRETO Nº 24.333/97, E NO INCISO V DO ART. 64 DO DECRETO Nº 24.569, DE 31 DE JULHO DE 1997.">CE - CEARÁ</option>
+                    <option value="CREDITO PRESUMIDO CONF. CONVÊNIO ICMS 106/96 COMBINADO A ALINEA 'B' DO INCISO III DO ART. 270 DO RICMS/BA">BA - BAHIA</option>
+                    <option value="CREDITO PRESUMIDO CONF. DECRETO Nº 27815 DE 24/01/2001 COMBINADO AO CONVÊNIO ICMS 106/96.">RJ - RIO DE JANEIRO</option>
+                    <option value="CREDITO PRESUMIDO - TRANSPORTADORA NÃO OBRIGADO A INSCRIÇÃO DENTRO DO ESTADO DO PE CONF. ALÍNEA 'E' DO INC XI DO ART. 35 DO DECRETO 14876/91.">PE - PERNAMBUCO</option>
+                    <option value="CREDITO PRESUMIDO - PRESTADOR DE SERVIÇO DE TRANSPORTE NÃO OBRIGADO À INSCRIÇÃO NO CCICMS - CONF. § 3º, ART. 25 DO ANEXO II DO RICMS/SC">SC - SANTA CATARINA</option>
+                    <option value="CREDITO PRESUMIDO - PRESTADOR NÃO OBRIGADO À INSCRIÇÃO NESTA UF. ALINEA C, INC. III, ART. 56 DO RICMS/PI">PI - PIAUÍ</option>
+                    <option value="CREDITO PRESUMIDO - INCISO II, § 1º, DO ARTIGO 64 DO RCTE-GO">GO - GOIÁS</option>
+                    <option value="CREDITO PRESUMIDO - INCISO 37 DO ARTIGO 107 DO RICMS/ES">ES - ESPIRITO SANTO</option>
+                    <option value="CREDITO PRESUMIDO - INCISO § 3º DO ARTIGO 11 DO ANEXO III DO RICMS-SP">SP - SÃO PAULO</option>
                   </select>
 
 
